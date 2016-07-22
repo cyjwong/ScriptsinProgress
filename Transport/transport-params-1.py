18be201a2550418a1ff05d16a1575b7a3cf544de
cyjwong <wong@intranet.nscl.msu.edu>
2016-07-22 09:37:52 -0400

    Transport

---
+++ Transport/transport-params-1.py
@@ -0,0 +1,45 @@
+# Solenoid Field Strength (in Telsa)
+
+SolenoidStrength = 0.4
+
+# Lattice Length
+
+PeriodLength = 1.2 # (times 5)
+
+# Number of Periods
+
+NumberofPeriods = 5
+
+# Initial Radius
+
+InitialRadius = 0.008
+
+# set linear or nonlinear fields
+
+fieldtype = "lin"
+
+# set neutralization factor
+
+NeutralizationFactor = 0.75
+
+#set velocity correction method: 0 - no correction, 1 - dBdz only, 2 - dBdz + d2Edz2
+
+CorrectionMode = 1
+
+# Number of particles per grid (default = 400, adjust for quick checks)
+
+ParticleNumber = 400
+
+# Envelope Plot Limits:
+plotxmin = "e"
+plotxmax = "e"
+plotymin = 0.
+plotymax = "e"
+
+# Center of 1st Solenoid
+
+s4p2_zc  = PeriodLength / 2.
+
+# Number of steps per period
+
+nstep_period = 100
